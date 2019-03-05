a0bbf402176071f9ff6d61299861dba19628fba8
Thom Sweeney <skywriter@example.com>
2019-03-05 14:14:55 +0000

    Removed footer (temporary)

--- src/components/layout.js
+++ src/components/layout.js
@@ -27,7 +27,7 @@ const Layout = ({ children }) => (
           siteSubtitle={data.site.siteMetadata.subTitle}
         />
         <div className="container">{children}</div>
-        <Footer />
+
       </div>
     )}
   />
--- src/pages/index.js
+++ src/pages/index.js
@@ -6,84 +6,14 @@ import Image from '../components/image';
 import SEO from '../components/seo';
 
 const IndexPage = () => (
+  const post = this.props.data.wordpressPost;
   <Layout>
     <SEO
       title="Welcome"
       keywords={[`research`, `teaching`, `learning`, 'teach', 'learn']}
     />
     <h1>Hello.</h1>
-    <p>
-      This is the homepage for <strong>Researching Education</strong>. Lorem
-      ipsum dolor sit amet consectetur adipisicing elit. Ad repudiandae et id
-      reiciendis rerum reprehenderit doloremque perferendis beatae ipsum, qui
-      nobis tenetur quas dolorum, quis quisquam magnam accusantium aperiam vero
-      sunt laboriosam natus ipsam repellendus obcaecati! Tempore ipsam nostrum
-      adipisci esse, dolor libero dolores, similique beatae aperiam error omnis
-      tempora?
-    </p>
-    <h2>What is this?</h2>
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis
-      veritatis odio pariatur ut cupiditate tenetur sit dolor ex et eligendi in
-      maiores, asperiores rem accusantium eaque autem eveniet, necessitatibus
-      enim eius eum voluptates non at. Facilis odio beatae deserunt aperiam
-      illum repellendus libero labore. Ratione illo modi totam quia recusandae,
-      amet voluptatum vero praesentium voluptas, error in? Ipsum dolores itaque
-      laboriosam esse, doloribus quia quo similique dolorum. Eum quos est,
-      repellat adipisci distinctio omnis possimus odit voluptates mollitia
-      praesentium modi exercitationem quis iure. Nobis consectetur, enim,
-      dolores vitae praesentium hic, velit consequatur excepturi ea similique
-      facere aperiam voluptatibus possimus expedita.
-    </p>
 
-    <h2>Join the conversation</h2>
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur
-      aliquid aut quasi voluptates fugiat delectus quas! Dolore dolorum
-      voluptatibus maxime in optio voluptate impedit omnis quod aliquid, rem
-      voluptas hic aperiam ipsum ipsa. Delectus tenetur pariatur dolor et ut
-      minima, ullam voluptas aliquid repellendus incidunt, cumque fuga cum
-      laboriosam eos sit iste eum at. Minus dolorum neque necessitatibus ex,
-      quod magni architecto consequatur. Quis ipsa earum non nisi eius amet
-      distinctio at maiores, nulla ea exercitationem beatae saepe et! Saepe
-      dolore labore ad et esse eveniet dicta, cupiditate dignissimos earum,
-      aliquam iure possimus, odio hic? Dolor alias fugiat vel nemo, quod eum
-      amet repellendus mollitia esse molestias voluptatum odio facere quisquam
-      minima laborum modi saepe repudiandae ullam nam? Ullam, nemo explicabo
-      fugit doloribus harum praesentium sint eveniet, perferendis adipisci quas
-      iusto cum! Facilis a consectetur ut impedit accusamus. Modi explicabo
-      cumque quisquam corrupti ullam quis exercitationem ea fugiat maxime quae
-      fugit veniam enim doloremque, unde perspiciatis soluta a autem? Asperiores
-      laudantium repellendus quaerat, deleniti enim ratione provident repellat
-      vero illo nostrum odio. Cumque maiores explicabo voluptas ab eligendi
-      doloremque consequuntur. Eius ut cum rerum consequatur id adipisci!
-      Dignissimos illum id et! Illo deleniti, neque error explicabo similique
-      dolorem tempore dignissimos.
-    </p>
-    <h2>Are you a writer witha passion for educational </h2>
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur
-      aliquid aut quasi voluptates fugiat delectus quas! Dolore dolorum
-      voluptatibus maxime in optio voluptate impedit omnis quod aliquid, rem
-      voluptas hic aperiam ipsum ipsa. Delectus tenetur pariatur dolor et ut
-      minima, ullam voluptas aliquid repellendus incidunt, cumque fuga cum
-      laboriosam eos sit iste eum at. Minus dolorum neque necessitatibus ex,
-      quod magni architecto consequatur. Quis ipsa earum non nisi eius amet
-      distinctio at maiores, nulla ea exercitationem beatae saepe et! Saepe
-      dolore labore ad et esse eveniet dicta, cupiditate dignissimos earum,
-      aliquam iure possimus, odio hic? Dolor alias fugiat vel nemo, quod eum
-      amet repellendus mollitia esse molestias voluptatum odio facere quisquam
-      minima laborum modi saepe repudiandae ullam nam? Ullam, nemo explicabo
-      fugit doloribus harum praesentium sint eveniet, perferendis adipisci quas
-      iusto cum! Facilis a consectetur ut impedit accusamus. Modi explicabo
-      cumque quisquam corrupti ullam quis exercitationem ea fugiat maxime quae
-      fugit veniam enim doloremque, unde perspiciatis soluta a autem? Asperiores
-      laudantium repellendus quaerat, deleniti enim ratione provident repellat
-      vero illo nostrum odio. Cumque maiores explicabo voluptas ab eligendi
-      doloremque consequuntur. Eius ut cum rerum consequatur id adipisci!
-      Dignissimos illum id et! Illo deleniti, neque error explicabo similique
-      dolorem tempore dignissimos.
-    </p>
   </Layout>
 );
 
