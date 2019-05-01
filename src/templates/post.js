fa9413647051780a50edac18e51e9d33e110264f
Tommy <skywriter@gmail.com>
2019-05-01 10:35:50 +0100

    added cover image functionailty to the posts object

--- src/templates/post.js
+++ src/templates/post.js
@@ -13,10 +13,24 @@ class PostTemplate extends Component {
     const post = this.props.data.wordpressPost;
     // boolean: Is this article republished from an external source such as "The Conversation"?
     const isExternalArticle = post.acf.external_article;
+
+    // featured image: the image that appears at the beggining of the article
+    const featuredImage =
+      post.acf.post_cover_image_file.localFile.childImageSharp.fluid;
+
     // string: The full reference to the original article in APA format
     const articleReference = post.acf.reference;
     // string - URL: the url to the original academic paper (if any)
     const articleReferenceLink = post.acf.link_to_article;
+    //fixed sized portrait image of author
+    const authorImage =
+      post.acf.author_portrait.localFile.childImageSharp.fixed;
+    //author name
+    const authorName = post.acf.author_name;
+    // author biography
+    const authorBio = post.acf.author_biography;
+    // author institution
+    const authorInstitution = post.acf.author_institution_or_business;
 
     // disqus comment system setup
     const disqusShortname = 'researching-education';
@@ -26,24 +40,27 @@ class PostTemplate extends Component {
     };
     return (
       <Layout>
-        <div class="post">
-          <div class="post__header">
+        <div className="post">
+          <div className="post__header">
             <div
-              class="post__header-date"
+              className="post__header-date"
               dangerouslySetInnerHTML={{ __html: post.date }}
             />
             <h1
-              class="post__header-title"
+              className="post__header-title"
               dangerouslySetInnerHTML={{ __html: post.title }}
             />
           </div>
+
+            <Img fluid={featuredImage} objectPosition="50% 50%" fadeIn="1" className="post__featured-image"/>
+
           {/*
           <div class="issue">
-            <div class="issue__title">
+            <div className="issue__title">
               Issue <span class="issue__number">01</span>
             </div>
-            <div class="hline" />
-            <div class="issue__date">February 2019</div>
+            <div className="hline" />
+            <div className="issue__date">February 2019</div>
           </div>
           -*/}
 
@@ -51,25 +68,25 @@ class PostTemplate extends Component {
 
           <div class="post__author">
             <Img
-              fixed={post.acf.author_portrait.localFile.childImageSharp.fixed}
+              fixed={authorImage}
               objectFit="cover"
               className="post__author-picture"
             />
             <br />
             <span class="post__author--italic-underline">by</span>&nbsp;
             <span
-              class="post__author-name"
-              dangerouslySetInnerHTML={{ __html: post.acf.author_name }}
+              className="post__author-name"
+              dangerouslySetInnerHTML={{ __html: authorName }}
             />
             <div
-              class="post__author-bio"
-              dangerouslySetInnerHTML={{ __html: post.acf.author_biography }}
+              className="post__author-bio"
+              dangerouslySetInnerHTML={{ __html: authorBio }}
             />
-            <div class="post__author-contact">
+            <div className="post__author-contact">
               <div
-                class="post__author-contact--institution "
+                className="post__author-contact--institution "
                 dangerouslySetInnerHTML={{
-                  __html: post.acf.author_institution_or_business,
+                  __html: authorInstitution,
                 }}
               />
               <div
@@ -119,6 +136,16 @@ export const postQuery = graphql`
     wordpressPost(id: { eq: $id }) {
       title
       acf {
+        post_cover_image_file {
+          localFile {
+            childImageSharp {
+              fluid(maxHeight: 660) {
+                src
+                srcSet
+              }
+            }
+          }
+        }
         author_name
         author_email_address
         author_institution_or_business
