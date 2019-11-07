90eb9c53b250e3cb4b6b1885f1c6c65746e549e0
Thom Sweeney <skywriter@example.com>
2019-11-07 00:59:29 +0000

    fix(.env) Rotated mailgun api key and fixed .env file, removing qutations around variables.

--- functions/mailgun-test.js
+++ functions/mailgun-test.js
@@ -4,9 +4,10 @@ const sendThankYouEmail = async () =>
   new Promise((resolve, reject) => {
     console.log('Sending the email');
 
-    const apiKey = 'key-dbb6b82b3939d99f842052389829d27d';
-    const domain = 'mail.researchingeducation.com';
-    const host = 'api.eu.mailgun.net';
+    // const apiKey = 'key-dbb6b82b3939d99f842052389829d27d';
+    const apiKey = process.env.MAILGUN_KEY;
+    const domain = process.env.MAILGUN_DOMAIN;
+    const host = process.env.MAILGUN_HOST;
 
     const mailgun = Mailgun({
       apiKey,
@@ -16,10 +17,9 @@ const sendThankYouEmail = async () =>
 
     const mailData = {
       from: 'Researching Education <admin@mail.researchingeducation.com>',
-      to: 'charles.crook@nottingham.ac.uk',
+      to: 'skywriter@gmail.com',
       subject: 'Thank you for subscribing to Researching Education',
-      text:
-        'Do not respond to this email directly. This is another test email. Did you receive this Charles?',
+      text: 'Do not respond to this email directly.',
     };
 
     mailgun.messages().send(mailData, err => {
